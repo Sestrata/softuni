ca400d6b0e6cdcb523abaeeaa49fbdebf262ecb5
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-10 10:33:25 +0300

    Create 07. Search for a Number.js

---
+++ 5.2. Arrays Advanced - Exercise/07. Search for a Number.js
@@ -0,0 +1,18 @@
+function solve(collection, numbers) {
+
+    let elementsToCut = numbers[0];
+    let elementsToDel = numbers[1];
+    let elementsToFind = numbers[2];
+
+    let modifiedArr = collection.slice(0, elementsToCut);
+    modifiedArr.splice(0, elementsToDel);
+    let counter = 0;
+    let modifiedArrL = modifiedArr.length;
+
+    for (let i = 0; i < modifiedArrL; i++) {
+        if (elementsToFind === modifiedArr[i]) {
+            counter++;
+        }
+    }
+    console.log(`Number ${elementsToFind} occurs ${counter} times.`);
+}
