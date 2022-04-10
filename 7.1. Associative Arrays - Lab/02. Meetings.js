b24cfd797ef4288c7acd3d4c7f471ba118f9b802
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-10 11:10:26 +0300

    Create 02. Meetings.js

---
+++ 7.1. Associative Arrays - Lab/02. Meetings.js
@@ -0,0 +1,20 @@
+function solve(input) {
+
+    let calendar = {};
+
+    for (let entry of input) {
+        entry = entry.split(' ');
+        let weekday = entry[0];
+        let person = entry[1];
+
+        if (calendar.hasOwnProperty(weekday)) {
+            console.log(`Conflict on ${weekday}!`);
+        } else {
+            calendar[weekday] = person;
+            console.log(`Scheduled for ${weekday}`);
+        }
+    }
+    for (let assocKey in calendar) {
+        console.log(`${assocKey} -> ${calendar[assocKey]}`);
+    }
+}
