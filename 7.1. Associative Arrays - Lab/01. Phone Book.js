8e1ab275d9018f9741f5b73a08a0edf1d0442b5d
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-10 11:10:03 +0300

    Create 01. Phone Book.js

---
+++ 7.1. Associative Arrays - Lab/01. Phone Book.js
@@ -0,0 +1,13 @@
+function solve(input) {
+
+    let assocArr = {};
+
+    for (let entry of input) {
+        entry = entry.split(' ');
+        assocArr[entry[0]] = entry[1];
+    }
+
+    for (let assocKey in assocArr) {
+        console.log(`${assocKey} -> ${assocArr[assocKey]}`);
+    }
+}
