1a2b5ff825945f7897560f5371faaa618da5dd97
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-10 10:31:25 +0300

    Create 02. Distinct Array.js

---
+++ 5.2. Arrays Advanced - Exercise/02. Distinct Array.js
@@ -0,0 +1,11 @@
+function solve(input) {
+
+    let result = [];
+
+    for (let num of input) {
+        if (!result.includes(num)) {
+            result.push(num);
+        }
+    }
+    console.log(result.join(' '));
+}
