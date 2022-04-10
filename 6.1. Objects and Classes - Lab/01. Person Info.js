5fd3d34e2e8316304ffacef7c4228da8933c5967
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2022-04-10 10:43:05 +0300

    Create 01. Person Info.js

---
+++ 6.1. Objects and Classes - Lab/01. Person Info.js
@@ -0,0 +1,17 @@
+function solve(fName, lName, a) {
+    
+    // let person = {
+    //     firstName: fName,
+    //     lastName: lName,
+    //     age: a
+    // }
+    // return person
+
+    // another - creating an empty array first:
+    let person = {};
+    person.firstName = fName;
+    person.lastName = lName;
+    person.age = a;
+
+    return person;
+}
